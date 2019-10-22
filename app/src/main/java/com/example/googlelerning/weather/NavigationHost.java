84dc08e69944ae2deaf5ad104256ba1d5b2def95
Vitas1968 <dor1968@mail.ru>
2019-10-22 19:54:54 +0700

    метод замены фрагментов в активити

--- app/src/main/java/com/example/googlelerning/weather/NavigationHost.java
+++ app/src/main/java/com/example/googlelerning/weather/NavigationHost.java
@@ -1,4 +1,7 @@
 package com.example.googlelerning.weather;
 
+import androidx.fragment.app.Fragment;
+
 public interface NavigationHost {
+    void navigateTo(Fragment fragment, boolean addToBackstack);
 }
