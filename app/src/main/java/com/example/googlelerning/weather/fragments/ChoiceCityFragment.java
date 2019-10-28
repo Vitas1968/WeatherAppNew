c3a0b94eb4df33a5b6e9cfcb332c7aa680a5b4e7
Vitas1968 <dor1968@mail.ru>
2019-10-28 19:33:48 +0700

    фрагмент ввода города

--- app/src/main/java/com/example/googlelerning/weather/fragments/ChoiceCityFragment.java
+++ app/src/main/java/com/example/googlelerning/weather/fragments/ChoiceCityFragment.java
@@ -1,6 +1,97 @@
 package com.example.googlelerning.weather.fragments;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.example.googlelerning.weather.NavigationHost;
+import com.example.googlelerning.weather.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import java.util.Objects;
+
 
 public class ChoiceCityFragment extends Fragment {
+    private TextInputEditText inputCity;
+    private String city;
+    private MaterialButton viewWatherBtn;
+    private LinearLayout lLayout;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.choice_city_fragment, container, false);
+        view.setId(R.id.choice_city_fr);
+        view.setTag("choiceCityFragment");
+        ininViews(view);
+        setListenerEditText();
+        setClickListenerButton();
+        return view;
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setRetainInstance(true);
+    }
+
+    private void ininViews(View view) {
+        inputCity=view.findViewById(R.id.inputCity);
+        viewWatherBtn=view.findViewById(R.id.viewWatherBtn);
+    }
+
+    private void setClickListenerButton() {
+        viewWatherBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (city!=null) {
+                    ((NavigationHost) Objects.requireNonNull(getActivity())).navigateTo(new ShowWeatherFragment(), true,city);
+                } else {
+                    Snackbar.make(v, "City not input", Snackbar.LENGTH_LONG)
+                            .setAction("CLOSE", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                }
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.holo_red_light ))
+                            .show();
+                }
+            }
+        });
+    }
+
+    private void setListenerEditText() {
+        inputCity.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)){
+                    TextView tv = (TextView) v;
+                    city = tv.getText().toString();
+
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " должен реализовывать интерфейс OnTransmitCity");
+        }
+    }
 }
