0e309672bbfedd99b6683be9c7ef52d83e5a4124
Vitas1968 <dor1968@mail.ru>
2019-10-28 19:38:46 +0700

    класс загрузки данных с погодного сервера

--- app/src/main/java/com/example/googlelerning/weather/weather_processing/WeatherDataLoader.java
+++ app/src/main/java/com/example/googlelerning/weather/weather_processing/WeatherDataLoader.java
@@ -1,4 +1,46 @@
 package com.example.googlelerning.weather.weather_processing;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class WeatherDataLoader {
+    private static final String OPEN_WEATHER_API_KEY = "6ca9777de46f536071253935840cba46";
+    private static final String OPEN_WEATHER_API_URL =
+            "https://api.openweathermap.org/data/2.5/forecast?q=%s&mode=json&units=metric";
+    private static final String KEY = "x-api-key";
+    private static final String RESPONSE = "cod";
+    private static final int ALL_GOOD = 200;
+
+    public static JSONObject getJSONData(String city) {
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_API_URL, city));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.addRequestProperty(KEY, OPEN_WEATHER_API_KEY);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader
+                    (connection.getInputStream()));
+            StringBuilder rawData = new StringBuilder(1024);
+            String tempVariable;
+
+            while ((tempVariable = reader.readLine()) != null) {
+                rawData.append(tempVariable).append("\n");
+            }
+
+            reader.close();
+
+            JSONObject jsonObject = new JSONObject(rawData.toString());
+            if(jsonObject.getInt(RESPONSE) != ALL_GOOD) {
+                return null;
+            } else {
+                return jsonObject;
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            return null;
+        }
+    }
 }
