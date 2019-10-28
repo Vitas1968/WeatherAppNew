1c4b63c40b5563c6c25269fda1ec742a156b8540
Vitas1968 <dor1968@mail.ru>
2019-10-28 19:40:13 +0700

    главная(и единственная)активность

--- app/src/main/java/com/example/googlelerning/weather/MainActivity.java
+++ app/src/main/java/com/example/googlelerning/weather/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.googlelerning.weather;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import com.example.googlelerning.weather.fragments.LoginFragment;
+import com.example.googlelerning.weather.fragments.ShowWeatherFragment;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationHost {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container_fragment, new LoginFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack, String city) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container_fragment, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+
+        if (fragment instanceof ShowWeatherFragment){
+            ((ShowWeatherFragment) fragment).updateWeatherData(city);
+        }
     }
 }
