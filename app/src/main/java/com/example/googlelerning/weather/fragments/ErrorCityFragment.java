9d56eeec3bfe3e19607dbd1e3feef8c4de62b2e8
Vitas1968 <dor1968@mail.ru>
2019-10-28 19:34:30 +0700

    фрагмент если город не введен или введен неверно

--- app/src/main/java/com/example/googlelerning/weather/fragments/ErrorCityFragment.java
+++ app/src/main/java/com/example/googlelerning/weather/fragments/ErrorCityFragment.java
@@ -1,6 +1,27 @@
 package com.example.googlelerning.weather.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.googlelerning.weather.R;
+
 public class ErrorCityFragment extends Fragment {
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.error_city_fragment, container, false);
+        view.setId(R.id.err_city_fr);
+        view.setTag("errorCityFragment");
+        return view;
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setRetainInstance(true);
+    }
 }
