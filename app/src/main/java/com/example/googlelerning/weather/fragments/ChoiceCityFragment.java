2931ec2ba17102663ad8d9b0e39754addc854eaf
Vitas1968 <dor1968@mail.ru>
2019-10-29 17:37:12 +0700

    подчистил код по рекомендациям варнингов

--- app/src/main/java/com/example/googlelerning/weather/fragments/ChoiceCityFragment.java
+++ app/src/main/java/com/example/googlelerning/weather/fragments/ChoiceCityFragment.java
@@ -1,12 +1,10 @@
 package com.example.googlelerning.weather.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,7 +21,6 @@ public class ChoiceCityFragment extends Fragment {
     private TextInputEditText inputCity;
     private String city;
     private MaterialButton viewWatherBtn;
-    private LinearLayout lLayout;
 
     @Override
     public View onCreateView(
@@ -84,14 +81,4 @@ public class ChoiceCityFragment extends Fragment {
         });
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        try {
-
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " должен реализовывать интерфейс OnTransmitCity");
-        }
-    }
 }
