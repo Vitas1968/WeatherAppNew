9dec230556943d5b43a8788cbe8a4b1a8f957819
Vitas1968 <dor1968@mail.ru>
2019-10-31 15:13:02 +0700

    получение сервисом погоды с сервера и возрат результата вызвавшему клиенту

--- app/src/main/java/com/example/googlelerning/weather/weather_processing/WeatherLoaderService.java
+++ app/src/main/java/com/example/googlelerning/weather/weather_processing/WeatherLoaderService.java
@@ -3,14 +3,38 @@ package com.example.googlelerning.weather.weather_processing;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import com.example.googlelerning.weather.fragments.ShowWeatherFragment;
+
+import org.json.JSONObject;
 
 public class WeatherLoaderService extends Service {
-    public WeatherLoaderService() {
-    }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String city=intent.getStringExtra(ShowWeatherFragment.PARAM_CITY);
+        getJsonWeather(city);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void getJsonWeather(String city) {
+        final String fCity=city;
+        new Thread() {
+            @Override
+            public void run() {
+              JSONObject  jsonObject = WeatherDataLoader.getJSONData(fCity);
+              if (jsonObject!=null) {
+                  String jsonString =jsonObject.toString();
+                  Intent intent = new Intent(ShowWeatherFragment.BROADCAST_ACTION);
+                  intent.putExtra("jsonObject",jsonString );
+                  sendBroadcast(intent);
+              }
+
+            }
+        }.start();
     }
 }
