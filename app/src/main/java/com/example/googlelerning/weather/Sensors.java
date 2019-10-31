60c0e561e87abea83af6d70f652cfa4ab69902ef
Vitas1968 <dor1968@mail.ru>
2019-10-31 17:32:05 +0700

    реализация класса Sensors

--- app/src/main/java/com/example/googlelerning/weather/Sensors.java
+++ app/src/main/java/com/example/googlelerning/weather/Sensors.java
@@ -3,10 +3,8 @@ package com.example.googlelerning.weather;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-
-
 import java.util.ArrayList;
-import java.util.List;
+
 
 import static android.content.Context.SENSOR_SERVICE;
 
@@ -14,8 +12,6 @@ public class Sensors {
 
     private Context context;
     private SensorManager sensorManager;
-    private Sensor sensorTemperature;
-    private Sensor sensorHumidity;
     private ArrayList<Sensor> sensorList;
 
     public Sensors(Context context){
@@ -27,12 +23,14 @@ public class Sensors {
     public ArrayList<Sensor> getSensors() {
         sensorManager = (SensorManager) context.getSystemService(SENSOR_SERVICE);
         assert sensorManager != null;
-        sensorTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
-        sensorHumidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        Sensor sensorTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        Sensor sensorHumidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
         sensorList.add(sensorTemperature);
         sensorList.add(sensorHumidity);
         return sensorList;
     }
 
-
+    public SensorManager getSensorManager() {
+        return sensorManager;
+    }
 }
