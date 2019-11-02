48b58f486adf0940fe83137d86641b7bd3784554
Vitas1968 <dor1968@mail.ru>
2019-11-02 13:09:41 +0700

    то же

--- app/src/main/java/com/example/googlelerning/weather/NameFildsSettings.java
+++ app/src/main/java/com/example/googlelerning/weather/NameFildsSettings.java
@@ -1,4 +1,7 @@
 package com.example.googlelerning.weather;
 
 public interface NameFildsSettings {
+    String SETTINS_FILE_NAME="Settings_file";
+    String CITY_NAME_SETTING="city_name";
+    String USER_LOGIN_SETTING="user_login";
 }
