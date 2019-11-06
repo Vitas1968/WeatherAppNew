070ecdd3c683bf20373aeeb12e07451c6b522b7c
Vitas1968 <dor1968@mail.ru>
2019-11-06 20:17:38 +0700

    класс фрагмента  об авторе

--- app/src/main/java/com/example/googlelerning/weather/fragments/InfoAutorDrawableFragment.java
+++ app/src/main/java/com/example/googlelerning/weather/fragments/InfoAutorDrawableFragment.java
@@ -1,6 +1,20 @@
 package com.example.googlelerning.weather.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.googlelerning.weather.R;
+
 public class InfoAutorDrawableFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.info_author_drawable_fragment, container, false);
+        return view;
+    }
 }
