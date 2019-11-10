08e1cac47710cafa24dd4aa63f6472b91227f024
Vitas1968 <dor1968@mail.ru>
2019-11-10 10:46:32 +0700

    главная модель данных запроса

--- app/src/main/java/com/example/googlelerning/weather/rest/entities/WeatherRequestRestModel.java
+++ app/src/main/java/com/example/googlelerning/weather/rest/entities/WeatherRequestRestModel.java
@@ -1,4 +1,11 @@
 package com.example.googlelerning.weather.rest.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class WeatherRequestRestModel {
+    @SerializedName("cod") public String codSuccessfully;
+    @SerializedName("message") public int message;
+    @SerializedName("cnt") public int cnt;
+    @SerializedName("list") public WeatherUpdateModel[] mWeatherUpdateModels;
+    @SerializedName("city") public CityInfoModel mCityInfoModel;
 }
