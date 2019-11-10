fca09903203076f414a1dd7404b58b764a674703
Vitas1968 <dor1968@mail.ru>
2019-11-10 14:20:10 +0700

    неализация интерфейса

--- app/src/main/java/com/example/googlelerning/weather/rest/IOpenWeather.java
+++ app/src/main/java/com/example/googlelerning/weather/rest/IOpenWeather.java
@@ -1,4 +1,15 @@
 package com.example.googlelerning.weather.rest;
 
+import com.example.googlelerning.weather.rest.entities.WeatherRequestRestModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface IOpenWeather {
+    @GET("data/2.5/forecast")
+    Call<WeatherRequestRestModel> loadWeather(@Query("q") String city,
+                                              @Query("appid") String keyApi,
+                                              @Query("mode") String jsonMode,
+                                              @Query("units") String units);
 }
