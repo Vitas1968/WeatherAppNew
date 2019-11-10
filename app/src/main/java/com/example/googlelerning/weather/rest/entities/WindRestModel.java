e6a414457aeb7cf494e0b06e66c8723b623857e6
Vitas1968 <dor1968@mail.ru>
2019-11-10 11:40:16 +0700

    реализация модели ветер

--- app/src/main/java/com/example/googlelerning/weather/rest/entities/WindRestModel.java
+++ app/src/main/java/com/example/googlelerning/weather/rest/entities/WindRestModel.java
@@ -1,4 +1,16 @@
 package com.example.googlelerning.weather.rest.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class WindRestModel {
+    @SerializedName("speed") private float speed;
+    @SerializedName("deg") private int deg;
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public int getDeg() {
+        return deg;
+    }
 }
