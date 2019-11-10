c3d883d3f77e3ff8a6f1c554c0741e757fc187af
Vitas1968 <dor1968@mail.ru>
2019-11-10 11:49:30 +0700

    реализация модели

--- app/src/main/java/com/example/googlelerning/weather/rest/entities/WeatherUpdateModel.java
+++ app/src/main/java/com/example/googlelerning/weather/rest/entities/WeatherUpdateModel.java
@@ -1,4 +1,42 @@
 package com.example.googlelerning.weather.rest.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class WeatherUpdateModel {
+
+    @SerializedName("dt") private long dt;
+    @SerializedName("main") private WeatherMainRestModel weatherMain;
+    @SerializedName("weather") private WeatherModel [] weatherModel;
+    @SerializedName("clouds") private CloudsRestModel clouds;
+    @SerializedName("wind") private WindRestModel wind;
+    @SerializedName("sys") private SysRestModel sys;
+    @SerializedName("dt_txt") private String dt_txt;
+
+    public long getDt() {
+        return dt;
+    }
+
+    public WeatherMainRestModel getWeatherMain() {
+        return weatherMain;
+    }
+
+    public WeatherModel[] getWeatherModel() {
+        return weatherModel;
+    }
+
+    public CloudsRestModel getClouds() {
+        return clouds;
+    }
+
+    public WindRestModel getWind() {
+        return wind;
+    }
+
+    public SysRestModel getSys() {
+        return sys;
+    }
+
+    public String getDt_txt() {
+        return dt_txt;
+    }
 }
