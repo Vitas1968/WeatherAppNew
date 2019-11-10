55da259aa3e1dc3a8f5290570ca7264b1f4a1f93
Vitas1968 <dor1968@mail.ru>
2019-11-10 11:04:56 +0700

    модель City

--- app/src/main/java/com/example/googlelerning/weather/rest/entities/CityInfoModel.java
+++ app/src/main/java/com/example/googlelerning/weather/rest/entities/CityInfoModel.java
@@ -1,4 +1,47 @@
 package com.example.googlelerning.weather.rest.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CityInfoModel {
+
+    @SerializedName("id") private int id;
+    @SerializedName("name") private String name;
+    @SerializedName("coord") private CoordRestModel coordinate;
+    @SerializedName("country") private String country;
+    @SerializedName("population") private int population;
+    @SerializedName("timezone") private int timezone;
+    @SerializedName("sunrise") private long sunrise;
+    @SerializedName("sunset") private long sunset;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CoordRestModel getCoordinate() {
+        return coordinate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public int getTimezone() {
+        return timezone;
+    }
+
+    public long getSunrise() {
+        return sunrise;
+    }
+
+    public long getSunset() {
+        return sunset;
+    }
 }
