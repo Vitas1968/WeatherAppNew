050a99952600df09d8f68b518b0ed29fe0477dd9
Vitas1968 <dor1968@mail.ru>
2019-11-10 14:21:31 +0700

    изменение реализации под Retrofit

--- app/src/main/java/com/example/googlelerning/weather/fragments/ShowWeatherFragment.java
+++ app/src/main/java/com/example/googlelerning/weather/fragments/ShowWeatherFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
@@ -19,11 +21,15 @@ import com.example.googlelerning.weather.R;
 import com.example.googlelerning.weather.recicler.BuildDataForRecicler;
 import com.example.googlelerning.weather.recicler.DataClass;
 import com.example.googlelerning.weather.recicler.RecyclerViewAdapter;
-import com.example.googlelerning.weather.weather_processing.WeatherDataLoader;
+import com.example.googlelerning.weather.rest.OpenWeatherRepo;
+import com.example.googlelerning.weather.rest.entities.WeatherRequestRestModel;
 import org.json.JSONException;
-import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ShowWeatherFragment extends Fragment {
     private final Handler handler = new Handler();
@@ -31,7 +37,9 @@ public class ShowWeatherFragment extends Fragment {
     private TextView placeName;
     private ArrayList<DataClass> list = new ArrayList<>();
     private Context mContext;
-
+    private static final String OPEN_WEATHER_API_KEY = "6ca9777de46f536071253935840cba46";
+    private static final String JSON_MODE = "json";
+    private static final String UNITS = "metric";
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -55,40 +63,30 @@ public class ShowWeatherFragment extends Fragment {
     }
 
     public void updateWeatherData(final String city) {
-        new Thread() {
-            @Override
-            public void run() {
-                final JSONObject jsonObject = WeatherDataLoader.getJSONData(city);
-
-                if(jsonObject == null) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            navigateErrorFragment();
+        OpenWeatherRepo.getSingleton().getAPI().loadWeather(city + ",ru",
+                OPEN_WEATHER_API_KEY ,JSON_MODE, UNITS)
+                .enqueue(new Callback<WeatherRequestRestModel>() {
+                    @Override
+                    public void onResponse(@NonNull Call<WeatherRequestRestModel> call,
+                                           @NonNull Response<WeatherRequestRestModel> response) {
+                        if (response.body() != null && response.isSuccessful()) {
+                            launch(response.body());
                         }
-                    });
-                } else {
-
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            launch(jsonObject);
-                        }
-                    });
-                }
-            }
-        }.start();
+                    }
+                    @Override
+                    public void onFailure(Call<WeatherRequestRestModel> call, Throwable t) {
+                        Toast.makeText(getContext(), getString(R.string.network_error),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 
-    private void launch(JSONObject jsonObject){
-        try {
-            list= new BuildDataForRecicler(jsonObject,mContext).createListData();
-            setPlaceName(list);
+    private void launch(WeatherRequestRestModel weatherRequestRestModel){
+
+            list= new BuildDataForRecicler(weatherRequestRestModel,mContext).createListData();
+            setPlaceName(weatherRequestRestModel);
             initRecyclerView();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
     private void initRecyclerView() {
         LinearLayoutManager layoutManager = new LinearLayoutManager(mContext);
@@ -101,8 +99,10 @@ public class ShowWeatherFragment extends Fragment {
         recycView.addItemDecoration(itemDecoration);
         recycView.setAdapter(adapter);
     }
-    private void setPlaceName(ArrayList<DataClass> list){
-        placeName.setText(list.get(0).getCityField());
+    private void setPlaceName(WeatherRequestRestModel weatherRequestRestModel){
+        String cityName= weatherRequestRestModel.mCityInfoModel.getName()+
+                " , "+weatherRequestRestModel.mCityInfoModel.getCountry();
+        placeName.setText(cityName);
     }
 
     private void navigateErrorFragment(){
