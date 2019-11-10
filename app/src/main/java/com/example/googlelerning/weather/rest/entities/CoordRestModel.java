e2c372f04ba0a6225c531a0b2c97891ebfac218a
Vitas1968 <dor1968@mail.ru>
2019-11-10 11:04:31 +0700

    реализация

--- app/src/main/java/com/example/googlelerning/weather/rest/entities/CoordRestModel.java
+++ app/src/main/java/com/example/googlelerning/weather/rest/entities/CoordRestModel.java
@@ -1,4 +1,16 @@
 package com.example.googlelerning.weather.rest.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CoordRestModel {
+    @SerializedName("lon") public float lon;
+    @SerializedName("lat") public float lat;
+
+    public float getLon() {
+        return lon;
+    }
+
+    public float getLat() {
+        return lat;
+    }
 }
