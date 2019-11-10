bcd4b09d98c2fb4a24de55ca352ff225455ef55f
Vitas1968 <dor1968@mail.ru>
2019-11-10 14:20:44 +0700

    реализация класса репозитория

--- app/src/main/java/com/example/googlelerning/weather/rest/OpenWeatherRepo.java
+++ app/src/main/java/com/example/googlelerning/weather/rest/OpenWeatherRepo.java
@@ -1,4 +1,38 @@
 package com.example.googlelerning.weather.rest;
 
+import com.example.googlelerning.weather.rest.entities.WeatherUpdateModel;
+import com.google.gson.annotations.SerializedName;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class OpenWeatherRepo {
+    private static OpenWeatherRepo singleton = null;
+    private IOpenWeather API;
+
+    private OpenWeatherRepo() {
+        API = createAdapter();
+    }
+
+    public static OpenWeatherRepo getSingleton() {
+        if(singleton == null) {
+            singleton = new OpenWeatherRepo();
+        }
+
+        return singleton;
+    }
+
+    public IOpenWeather getAPI() {
+        return API;
+    }
+
+    private IOpenWeather createAdapter() {
+        Retrofit adapter = new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return adapter.create(IOpenWeather.class);
+    }
+
 }
