b130529e3da047e620d924f50c50734a7e641a93
Vitas1968 <dor1968@mail.ru>
2019-11-14 19:59:17 +0700

    новые классы для работы с базой данных: класс для работы с базой DatabaseHelper, класс для операций с таблицей WeatherDataTable и класс преобразования данных из БД для рециклера ConvertDataFromDB

---
+++ app/src/main/java/com/example/googlelerning/weather/database/ConvertDataFromDB.java
@@ -0,0 +1,4 @@
+package com.example.googlelerning.weather.database;
+
+public class ConvertDataFromDB {
+}
---
+++ app/src/main/java/com/example/googlelerning/weather/database/DatabaseHelper.java
@@ -0,0 +1,6 @@
+package com.example.googlelerning.weather.database;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+}
